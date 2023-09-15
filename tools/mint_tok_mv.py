397b8a7fb6c5d90fa1078b191a231725116bc01a
house <housesellout@gmail.com>
2023-09-14 23:14:25 -0400

    … misc cleanup from previous commit (ready for public release) 	- added ‘HOW-TO’
    # house_091423
    '''
        HOW-TO use this code from CLI (command line interface)
            1) install python3 on your system
                ref: https://letmegooglethat.com/?q=how+to+install+python3+on+my+system
            2) open up the CLI and install web3.py
                $ python3 -m pip install web3
            3) set variables below
                'sender_address' = the address of the wallet you are using (like in MM)
                'sender_secret'  = the private key of the wallet you are using (like in MM)
            4) run the script from CLI
                $ python3 mint_tok_mv.py
                
            short-hand:
                $ python3 -m pip install web3
                $ python3 mint_tok_mv.py
    '''

--- tools/mint_tok_mv.py
+++ tools/mint_tok_mv.py
@@ -1,3 +1,21 @@
+# house_091423
+'''
+    HOW-TO use this code from CLI (command line interface)
+        1) install python3 on your system
+            ref: https://letmegooglethat.com/?q=how+to+install+python3+on+my+system
+        2) open up the CLI and install web3.py
+            $ python3 -m pip install web3
+        3) set variables below
+            'sender_address' = the address of the wallet you are using (like in MM)
+            'sender_secret'  = the private key of the wallet you are using (like in MM)
+        4) run the script from CLI
+            $ python3 mint_tok_mv.py
+            
+        short-hand:
+            $ python3 -m pip install web3
+            $ python3 mint_tok_mv.py
+'''
+
 __fname = 'mint_tok_mv'
 __filename = __fname + '.py'
 cStrDivider = '#================================================================#'
@@ -5,25 +23,76 @@ print('', cStrDivider, f'START _ {__filename}', cStrDivider, sep='\n')
 print(f'GO {__filename} -> starting IMPORTs and globals decleration')
 
 #------------------------------------------------------------#
-#   IMPORTS (default)                                        #
+#   IMPORTS                                                  #
 #------------------------------------------------------------#
-import sys, json
+import sys
 from datetime import datetime
-#import requests
+from web3 import Web3
 #import inspect # this_funcname = inspect.stack()[0].function
 
 # support import from parent dir
-#   add parent dir of this file to sys.path
 #parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#sys.path.append(parent_dir)
-
-#------------------------------------------------------------#
-#   IMPORTS                                                  #
-#------------------------------------------------------------#
-from web3 import Web3
+#sys.path.append(parent_dir) # add parent dir of this file to sys.path
 
 #------------------------------------------------------------#
 #   GLOBALS
+#------------------------------------------------------------#
+# contract address & sender keys
+contract_address = '0xA1BEe1daE9Af77dAC73aA0459eD63b4D93fC6d29' # ᨓᨆ (ᨓᨆ)
+sender_address = "0xYourSenderAddress"
+sender_secret = "sender_address_private_key"
+
+# function hex (decompiled) & contract ABI (re-compiled) from contract_address
+#   byte-code: https://scan.pulsechain.com/address/0xA1BEe1daE9Af77dAC73aA0459eD63b4D93fC6d29/contracts#address-tabs
+#   decompile: https://library.dedaub.com/decompile
+#   recompile: https://remix.ethereum.org/
+func_hex = "0xa4566950"
+contract_abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"owner","type":"address"},{"indexed":True,"internalType":"address","name":"spender","type":"address"},{"indexed":False,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":True,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"from","type":"address"},{"indexed":True,"internalType":"address","name":"to","type":"address"},{"indexed":False,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+#------------------------------------------------------------#
+#   FUNCTION SUPPORT
+#------------------------------------------------------------#
+def mint_mv():
+    print('ENTER - mint_mv')
+    try:
+        print('# connect to pulse chain')
+        #w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'))
+        w3 = Web3(Web3.HTTPProvider('https://rpc.pulsechain.com'))
+
+        print('# Check if connected')
+        if w3.isConnected(): print(" Connected to PulseChain mainnet")
+        else: print(" Failed to connect to PulseChain mainnet")
+
+        print('# get the contract w/ address & abi')
+        contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+        
+        print('# Prepare the transaction data')
+        tx_data = {
+            #"value": w3.toWei(1, "ether"),  # Specify the value you want to send with the transaction
+            "to": contract_address,
+            "chainId": 369, # 369 = pulsechain Mainnet... required for replay-protection (EIP-155)
+            "data": func_hex, # Call the custom function hex value on the contract
+            "gas": 20000000,  # Adjust the gas limit as needed
+            "gasPrice": w3.toWei("4000000", "gwei"),  # Adjust the gas price as needed
+            "nonce": w3.eth.getTransactionCount(sender_address)
+        }
+
+        print(f'# Create a signed transaction _ tx_data: {tx_data}')
+        signed_tx = w3.eth.account.signTransaction(tx_data, private_key=sender_secret)
+
+        print('# Send the transaction')
+        tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        
+        print(f'# wait for mined receipt _ tx_hash: {tx_hash.hex()}')
+        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        
+        print(cStrDivider, f'Function "{func_hex}" executed successfully...\n tx_hash: {tx_hash.hex()}\n Transaction receipt: {tx_receipt}', cStrDivider, sep='\n')
+        
+    except Exception as e:
+        print(cStrDivider, f'\n*Exception Caught*\n _ Error: {e}\n', cStrDivider, sep='\n')
+
+#------------------------------------------------------------#
+#   DEFAULT SUPPORT                                          #
 #------------------------------------------------------------#
 READ_ME = f'''
     *EXAMPLE EXECUTION*
@@ -33,111 +102,6 @@ READ_ME = f'''
     *NOTE* INPUT PARAMS...
         nil
 '''
-
-# URL w/ RPC endpoint to connect to public Ethereum node (or local test node)
-#w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
-#w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'))
-
-# Replace with your contract address and ABI (Application Binary Interface)
-#contract_address = '0xYourContractAddress' # MV
-contract_abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"owner","type":"address"},{"indexed":True,"internalType":"address","name":"spender","type":"address"},{"indexed":False,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":True,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"from","type":"address"},{"indexed":True,"internalType":"address","name":"to","type":"address"},{"indexed":False,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-contract_address = '0xA1BEe1daE9Af77dAC73aA0459eD63b4D93fC6d29' # ᨓᨆ (ᨓᨆ)
-
-# set caller keys & gas params
-#sender_address = "0xYourSenderAddress"
-sender_address = '0xYourAddress'
-sender_secret = 'your_private_secret_key'
-#private_key = "0xYourPrivateKey"
-#gas_price = w3.toWei("10", "gwei")  # Replace with your desired gas price
-#gas_limit = 200000  # Replace with your desired gas limit
-
-def go_test():
-    print('ENTER - go_test')
-    # Call the 0xa4566950 function on the contract
-    try:
-        print('go # connect to pulse chain')
-        # connect to pulse chain
-        w3 = Web3(Web3.HTTPProvider('https://rpc.pulsechain.com'))
-
-        print('go # Check if connected')
-        # Check if connected
-        if w3.isConnected():
-            print("Connected to PulseChain mainnet")
-        else:
-            print("Failed to connect to PulseChain mainnet")
-            
-#        # get the contract w/ address & abi
-#        contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-#
-#        # Prepare the transaction & sign
-#        transaction = contract.functions.payableFunction().buildTransaction({
-#            "chainId": 369,  # 369 = pulsechain Mainnet
-#            "gasPrice": gas_price,
-#            "gas": gas_limit,
-#            "nonce": w3.eth.getTransactionCount(sender_address),
-#        })
-#        signed_transaction = w3.eth.account.signTransaction(transaction, private_key)
-#
-#        # Send the transaction & wait to be mined
-#        transaction_hash = w3.eth.sendRawTransaction(signed_transaction.rawTransaction)
-#        w3.eth.waitForTransactionReceipt(transaction_hash)
-#
-#        print(f"Transaction hash: {transaction_hash.hex()}")
-        
-        
-#        # Use the 'transact' method to send the transaction
-#        tx_hash = contract.functions['0xa4566950']().transact({'from': your_account_address, 'value': 0})
-#
-#        print('go 2')
-#        # Wait for the transaction to be mined
-#        w3.eth.waitForTransactionReceipt(tx_hash)
-#
-#        print('go 3')
-#        print(f'Function 0xa4566950 executed successfully.')
-
-        print('go # get the contract w/ address & abi')
-        # get the contract w/ address & abi
-        contract = w3.eth.contract(address=contract_address, abi=contract_abi)
-        
-        print('go # Prepare the transaction data')
-        # Prepare the transaction data
-        # Hex value of function decompiled from contract_address (ᨓᨆ)
-        #   byte-code: https://scan.pulsechain.com/address/0xA1BEe1daE9Af77dAC73aA0459eD63b4D93fC6d29/contracts#address-tabs
-        #   decompile: https://library.dedaub.com/decompile
-        func_hex = "0xa4566950"
-        tx_data = {
-            "nonce": w3.eth.getTransactionCount(sender_address),
-            "to": contract_address,
-            "data": func_hex,
-#            "gas": 200000,  # Adjust the gas limit as needed
-#            "gasPrice": w3.toWei("50", "gwei")  # Adjust the gas price as needed
-#            "gas": 25200,  # Adjust the gas limit as needed
-            "gas": 20000000,  # Adjust the gas limit as needed
-            "gasPrice": w3.toWei("4000000", "gwei"),  # Adjust the gas price as needed
-            #"value": w3.toWei(1, "ether")  # Specify the value you want to send with the transaction
-            "chainId": 369 # 369 = pulsechain Mainnet... required for replay-protection (EIP-155)
-        }
-
-        print(f'go # Create a signed transaction _ tx_data: {tx_data}')
-        # Create a signed transaction
-        signed_tx = w3.eth.account.signTransaction(tx_data, private_key=sender_secret)
-
-        print('go # Send the transaction')
-        # Send the transaction
-        tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-        
-        print(f'go # wait for mined receipt _ tx_hash: {tx_hash}')
-        # wait for mined receipt
-        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-        
-        print(f'Function "{func_hex}" executed successfully...\n tx_hash: {tx_hash.hex()}\n Transaction receipt: {tx_receipt}')
-        
-    except Exception as e:
-        print(f'Error: {e}')
-
-#------------------------------------------------------------#
-#   DEFAULT SUPPORT                                          #
-#------------------------------------------------------------#
 def wait_sleep(wait_sec : int, b_print=True): # sleep 'wait_sec'
     print(f'waiting... {wait_sec} sec')
     for s in range(wait_sec, 0, -1):
@@ -165,7 +129,7 @@ def go_main():
         exit(1)
 
     # execute procedural support
-    go_test()
+    mint_mv()
 
     # end
     print(f'\n\nRUN_TIME_START: {run_time_start}\nRUN_TIME_END:   {get_time_now()}\n')
@@ -174,9 +138,6 @@ if __name__ == "__main__":
     go_main()
 
 print('', cStrDivider, f'# END _ {__filename}', cStrDivider, sep='\n')
-
-
-
 
 '''
     known addresses minting MV
