8e7c225f1916f034d147ec2fcd3e30e8bd304712
house <housesellout@gmail.com>
2023-09-16 22:50:30 -0400

    … misc copy fix

--- _tools/calc_return_write.py
+++ _tools/calc_return_write.py
@@ -12,7 +12,7 @@
             $ python3 calc_return_write.py
             
         short-hand:
-            $ python3 -m pip install web3
+            $ python3 -m pip install requests
             $ python3 -m pip install web3
             $ python3 mint_tok_mv.py
             
