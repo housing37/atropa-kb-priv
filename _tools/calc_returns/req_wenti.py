31353207f68c9f3030aea47eecb084ecfee393dc
house <housesellout@gmail.com>
2023-09-29 23:20:28 -0400

    … missing from prev commit

--- _tools/calc_returns/req_wenti.py
+++ _tools/calc_returns/req_wenti.py
@@ -12,7 +12,7 @@ print(f'GO {__filename} -> starting IMPORTs and globals decleration')
 contract_address = '0xA537d6F4c1c8F8C41f1004cc34C00e7Db40179Cc'
 contract_symbol = '问题 (问题) _ wenti'
 
-alt_tok_addr_0 = '0x52a4682880E990ebed5309764C7BD29c4aE22deB' # 2,000,000 유 (YuContract) _ (ì = EC9CA0)
+alt_tok_addr_0 = '0x52a4682880E990ebed5309764C7BD29c4aE22deB' # 2,000,000 (YuContract) _ (ì = EC9CA0)
 alt_tok_addr_1 = '0x347BC40503E0CE23fE0F5587F232Cd2D07D4Eb89' # 1 Di (DiContract) _ (ç¬¬ä½ = E7ACACE4BD9C)
 alt_tok_vol_0 = 2000000 # 2,000,000 _ '_SafeMul(0x1e8480,' _ 'Need Approved 2,000,000 ì' (ì = EC9CA0)
 alt_tok_vol_1 = 1 # 1 _ '_SafeMul(1,' _ 'Need Approved 1 ç¬¬ä½' (ç¬¬ä½ = E7ACACE4BD9C)
